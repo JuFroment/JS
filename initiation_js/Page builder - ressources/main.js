5dc3309569ddac10092de08eeed37a2043661f2b
Julien Froment <ju.c.froment@gmail.com>
2021-11-28 13:06:49 +0100

    Début Exo page builder

---
+++ initiation_js/Page builder - ressources/main.js
@@ -0,0 +1,17 @@
+"use-strict";
+
+/***********
+ * VARIABLES
+ ***********/
+
+let content;
+
+/***********
+ * FUNCTIONS
+ ***********/
+
+/***********
+ * MAIN CODE
+ ***********/
+
+content = document.querySelector("#content");
