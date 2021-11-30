ce6449b0273a8b751cd87a55cb431a168dc4ca5d
Julien Froment <ju.c.froment@gmail.com>
2021-11-30 09:39:38 +0100

    début exo page builder

--- initiation_js/Page builder - ressources/main.js
+++ initiation_js/Page builder - ressources/main.js
@@ -5,13 +5,23 @@
  ***********/
 
 let content;
+let button;
 
 /***********
  * FUNCTIONS
  ***********/
+
+function generateHTML() {
+	console.log("ok");
+	content.innerHTML = "<h1 contenteditable='true'>Hello World</h1>";
+}
 
 /***********
  * MAIN CODE
  ***********/
 
 content = document.querySelector("#content");
+button = document.querySelectorAll("aside button");
+for (let i = 0; i < button.length; i++) {
+	button[i] = document.addEventListener("click", generateHTML);
+}
